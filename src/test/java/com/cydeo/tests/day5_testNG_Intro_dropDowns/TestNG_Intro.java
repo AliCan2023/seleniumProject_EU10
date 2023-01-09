429936c570e296af5588a0f02858902334245b66
Ali Can <alibugraoglu@gmail.com>
2023-01-09 22:57:11 +0300

    days5 TestNG_Intro

--- src/test/java/com/cydeo/tests/day5_testNG_Intro_dropDowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_Intro_dropDowns/TestNG_Intro.java
@@ -1,15 +1,41 @@
 package com.cydeo.tests.day5_testNG_Intro_dropDowns;
 
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class TestNG_Intro {
 
-    @Test
-    public void test1(){
+
+    @Test(priority = 1)
+    public void test1() {
 
         System.out.println("Test1 is running...");
 
+        // ASSERT EQUALS: compare 2 of the same things
+        String actual = "apple";
+        String expected = "apple";
+        Assert.assertEquals(actual,expected);
+    }
 
+    @Test(priority = 2)
+    public void test2() {
+        System.out.println("Test2 is running...");
+    }
+    @BeforeClass
+    public void setUp(){
+        System.out.println("---->BeforeClass is running....");
+    }
+    @AfterClass
+    public void tearDown(){
+        System.out.println("---->AfterClass is running....");
+    }
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println(" ---->Before method is running...");
+    }
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println(" -----> After method is running...");
     }
 
 }

09aa798161e732fafd36fafeec2e34e5848a66bd
Ali Can <alibugraoglu@gmail.com>
2023-01-10 01:01:57 +0300

    days5 TestNG_Intro

--- src/test/java/com/cydeo/tests/day5_testNG_Intro_dropDowns/Task4_VerifySimpleDropDown.java
+++ src/test/java/com/cydeo/tests/day5_testNG_Intro_dropDowns/Task4_VerifySimpleDropDown.java
@@ -1,7 +1,11 @@
 package com.cydeo.tests.day5_testNG_Intro_dropDowns;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -24,12 +28,26 @@ public class Task4_VerifySimpleDropDown {
     @Test
     public void simpleDropDownTest() {
 
-
-
-
         //3. Verify “Simple dropdown” default selected value is correct
+        Select simpleDropDown = new Select(driver.findElement(By.xpath("//select[@id='dropdown']")));
+
+        WebElement currentlySelectedOption = simpleDropDown.getFirstSelectedOption();
+        String actualSimpleDropDownText = currentlySelectedOption.getText();
+        String expectedSimpleDropDownText= "Please select an option";
+        //System.out.println(actualSimpleDropDownText);
+
         //Expected: “Please select an option”
+        Assert.assertEquals(actualSimpleDropDownText,expectedSimpleDropDownText,"Text is not matching!");
+
         //4. Verify “State selection” default selected value is correct
+        Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
         //Expected: “Select a State”
+        String expectedStateDropdownText = "Select a State";
+        String actualStateDropdownText=stateDropdown.getFirstSelectedOption().getText();
+
+        Assert.assertEquals(actualStateDropdownText,expectedStateDropdownText);
+        //Assert.assertEquals(stateDropdown.getFirstSelectedOption().getText(),"Select a State");
+
+
     }
 }

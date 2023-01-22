94b495b3c4e08343857e6888c8d24d96656fba8c
Ali Can <alibugraoglu@gmail.com>
2023-01-22 18:32:53 +0300

    day9_javaFaker

--- src/test/java/com/cydeo/tests/day9_javaFaker_testbase_driverUtility/JavaFakerPractice.java
+++ src/test/java/com/cydeo/tests/day9_javaFaker_testbase_driverUtility/JavaFakerPractice.java
@@ -14,9 +14,30 @@ public class JavaFakerPractice {
 
         Faker faker = new Faker();
 
-        System.out.println("faker.name().firstName() = " + faker.name().firstName());
-        System.out.println("faker.name().lastName() = " + faker.name().lastName());
-        System.out.println("faker.name().fullName() = " + faker.name().fullName());
+        System.out.println("faker.name().firstName() = "
+                + faker.name().firstName());
+        System.out.println("faker.name().lastName() = "
+                + faker.name().lastName());
+        System.out.println("faker.name().fullName() = "
+                + faker.name().fullName());
+        System.out.println("faker.address().city() = "
+                + faker.address().city());
+        //System.out.println("faker.numerify() = "
+        // + faker.numerify("###-###-##-##-###"));// pass the format and numerify;
+        System.out.println("faker.numerify(\"###-###-##-##\") = "
+                + faker.numerify("###-###-##-##"));
+        System.out.println("faker.numerify(\"534-###-##-33\") = "
+                + faker.numerify("534-###-##-33"));
+
+        System.out.println("faker.letterify(\"???-???-??-??\") = "
+                + faker.letterify("???-???-??-??yy"));
+        System.out.println("faker.bothify(\"#?#?-##??-#?\") = "
+                + faker.bothify("#?#?-##??-#?xxx"));
+        System.out.println("faker.finance().creditCard().replaceAll(\"-\",\"\") = "
+                + faker.finance().creditCard().replaceAll("-", ""));
+
+        System.out.println("faker.chuckNorris().fact() = "
+                + faker.chuckNorris().fact().replaceAll("Chuck Norris","Muhtar"));
 
     }
 }
